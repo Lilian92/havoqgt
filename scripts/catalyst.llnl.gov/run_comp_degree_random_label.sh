cec5f250008e023c9a6a02fb17265abb1072cbe8
ictxjan@gmail.com <an4@catalyst160.llnl.gov>
2019-06-03 08:43:43 -0700

    add scripts for generating graphs and for comparing degree/random labels performance

---
+++ scripts/catalyst.llnl.gov/run_comp_degree_random_label.sh
@@ -0,0 +1,31 @@
+if [ "$#" -ne 1 ]; then echo "Usage: $0 program-dir"; exit 1; fi
+
+program="$1"
+patterns[0]="/p/lustre1/an4/patterns/tree/0"
+
+if [ ! -d "$program" ]; then
+    echo "Error: program-dir not found"
+    exit 2
+fi
+
+#example: (../src is the directory of program)
+#./run_comp_degree_random_label ../src
+
+#scales=(28 31 34 37)
+#nodes=(2 16 128 1024)
+
+scales=(18 20 22 24)
+nodes=(2 2 2 2)
+
+#when flags count is 0, it's running with degree based label
+#when flags count is bigger than 0, it's running generating random label in range [0, flags_count)
+for i in 0 1 2 3; do
+    echo $i
+    for pattern in ${patterns[*]}; do
+        for flags_count in 0 2 4 8 16 32 64 128 256 512 1024; do
+            echo $flags_count
+            sbatch -N${nodes[i]}\
+                "--export=ARG=-i /dev/shm/rmat${scales[i]} -b /p/lustre1/an4/graph/rmat${scales[i]} -u $flags_count -p $pattern -o ./usr/results,P=$program/run_pattern_matching_beta_1.1" pattern_matching.sbatch
+        done
+    done
+done
---
+++ scripts/catalyst.llnl.gov/run_gen_rmat.sh
@@ -0,0 +1,23 @@
+if [ "$#" -ne 1 ]; then echo "Usage: $0 program-dir"; exit 1; fi
+
+program="$1"
+
+if [ ! -d "$program" ]; then
+    echo "Error: program-dir not found"
+    exit 2
+fi
+
+#scales=(28 31 34 37)
+#nodes=(2 16 128 1024)
+
+scales=(18 20 22 24 28)
+nodes=(2 2 2 2 2)
+
+#scales=(18)
+#nodes=(1)
+
+for i in {0..4}; do
+    echo $i, ${scales[i]}, ${nodes[i]}
+    sbatch -N${nodes[i]}\
+        "--export=ARG=-s ${scales[i]} -p 1 -f 1 -o /dev/shm/rmat${scales[i]} -b /p/lustre1/an4/graph/rmat${scales[i]},P=$program/generate_rmat" gen_rmat.sbatch
+done
